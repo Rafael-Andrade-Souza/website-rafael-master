a4e1e44d59ae2c81366e1c5847e402a4c42e5dea
Rafael-Andrade-Souza <116676943+Rafael-Andrade-Souza@users.noreply.github.com>
2022-11-09 20:57:02 -0300

    Register Fix

---
+++ pages/register.js
@@ -0,0 +1,75 @@
+
+import { Container, Box, Heading, Button, Link, FormControl, FormLabel, Input, Stack, Checkbox} from '@chakra-ui/react'
+import React from 'react'
+import Layout from '../components/layouts/postLayout'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Section from '../components/section'
+
+const registerPage = () => {
+    return(
+        <Layout>
+        <Container>
+        <Heading as="h2" variant="page-title">
+        Registration
+        </Heading>
+        <Section>
+        <Box>
+        <form>
+            <FormControl>
+                <FormLabel>Name</FormLabel>
+                <input type='string' placeholder='Your Name Here'/>
+            </FormControl>
+            <FormControl>
+                <FormLabel>E-Mail</FormLabel>
+                <Input type='email' placeholder='E-mail Here'/>
+            </FormControl>
+            <FormControl>
+                <FormLabel>Password</FormLabel>
+                <Input type ='password' placeholder='Password Here'/>
+            </FormControl>
+                
+                <Button rightIcon={<ChevronRightIcon />} fill="currentcolor">
+                Register
+                </Button>
+            
+            <Stack>
+                <Box>
+                    <Link href='/login'>Login</Link>
+                </Box>
+            </Stack>
+        </form>
+        </Box>
+        </Section>
+        </Container>
+        </Layout>
+    )
+}
+
+
+const registerForm = () => {
+    return (
+    <Box>
+        <form>
+            <FormControl>
+                <FormLabel>E-Mail</FormLabel>
+                <Input type='email' placeholder='E-mail Here' />
+            </FormControl>
+            <FormControl>
+                <FormLabel>Password</FormLabel>
+                <Input type ='password' placeholder='Password Here' />
+            </FormControl>
+
+            <Stack>
+                <Box>
+                    <Checkbox>Remember Me</Checkbox>
+                </Box>
+                <Box>
+                    <Link> Forgot you Password </Link>
+                </Box>
+            </Stack>
+        </form>
+        </Box>
+    )
+}
+
+export default registerPage
